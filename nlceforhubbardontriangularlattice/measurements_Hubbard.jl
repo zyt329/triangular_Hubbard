10cc2866a433bd1231dbe86dcf39e44b67091ed6
zyt329 <zyt_329icloud.com>
2022-04-22 02:28:33 -0700

    Added Hubbard.jl and meansurement_Hubbard.jl, revised NLCE script to work with Hubbard model. The NLCE script gives result without coding bug, but don't know if the result is reliable.

--- nlceforhubbardontriangularlattice/measurements_Hubbard.jl
+++ nlceforhubbardontriangularlattice/measurements_Hubbard.jl
@@ -13,7 +13,7 @@
     Output:
         [E, Quantity1, Quantity2...]. E, Quantity1, Quantity2... are all arrays of length 2^N
 """
-function E_Quants(;T::Float64, N::Int64, sectors_info::Dict{Symbol,Any}, bonds::Array{Array{Int,1},1})
+function E_Quants(;N::Int64, sectors_info::Dict{Symbol,Any}, bonds::Array{Array{Int,1},1})
     #P::Float64 = 0
     E = Float64[]
     Esq = Float64[]
@@ -23,16 +23,107 @@ function E_Quants(;T::Float64, N::Int64, sectors_info::Dict{Symbol,Any}, bonds::
     for N_up=0:N, N_dn=0:N
         #println("N_up=$N_up, N_dn=$N_dn")
         # construct hamiltonian for the sector and diagonalize
-        H_m = H_sector(;t=t, U=U, N=N, N_up=N_up, N_dn=N_dn, sectors_info=sectors_info, bonds=bonds)
+        H_m = H_sector(;t=1.0, U=1.0, N=N, N_up=N_up, N_dn=N_dn, sectors_info=sectors_info, bonds=bonds)
         (Es, states) = eigen(Hermitian(Matrix(H_m)))
         # calculate quantiies for each eigenstate
-        for (ind, state)) in enumerate(states)
+        for (ind, En) in enumerate(Es)
+            push!(E, En)
             push!(Esq, Es[ind]^2)
             push!(M, ((N_up-N_dn)*1/2))
             push!(Msq, ((N_up-N_dn)*1/2)^2)
             push!(N_tot, N_up+N_dn)
         end
-
     end
-    return (P,E,Esq,M,Msq)
+    return [E, Esq, M, Msq, N_tot]
 end
+
+
+
+using JLD
+using DelimitedFiles
+
+
+#=sample = [Float64[], Float64[], Float64[], Float64[], Float64[]]
+for i in 1:5
+    push!(sample[i], i)
+    push!(sample[i], 2i)
+end=#
+
+
+"""
+    prints the Quantities to a .dat file for save in a given folder for each cluster. Saved name is name_Ntop
+    Input:
+        Quantities::Array{Array{Float64,1},1} : An array of Arrays of quantities
+            example: sample = [Float64[], Float64[], Float64[],     Float64[], Float64[]]
+                for i in 1:5
+                    push!(sample[i], i)
+                    push!(sample[i], 2i)
+                end
+        Quant_names::Array{Any,2} : An array of names of quantities
+            example: ['E' "Esq" 'M' "Msq" "N_tot"]
+        name::string : name of the saved files
+        NTOP::Int64 : topological number of the cluster whose quantities we're saving.
+"""
+function printing(Quantities; Quant_names = quant_names, name = "test", NTOP::Int64,N::Int64)
+    #println(Quantities)
+    open(
+        "D:/UC Davis/Research/triangular_Hubbard/desktop_files/triangular_Hubbard/nlceforhubbardontriangularlattice/Hubbard_data/"*name*"_$(N)_$(NTOP)"*".dat",
+        "w",
+    ) do io
+        writedlm(io, quant_names)
+        for (i,Quant) in enumerate(Quantities[1])
+            writedlm(io, [Quantities[n][i] for n in 1:length(Quantities)]')
+        end
+    end
+end
+
+
+# Quantities = E_Quants(;N=N, sectors_info=sectors_info, bonds=bonds)
+
+# printing(Quantities; name = "test_print2txt", NTOP=1)
+
+
+"""
+    do thermal average of quantities read from the file created by E_Quants() and printing()
+    return:
+        An array of thermal average of quantities at temperature of T
+"""
+function thermal_avg(;T::Real, name::String = "test", NTOP::Int64, N::Int64)
+    # read in the data from file
+    global quantities
+    open(
+        "D:/UC Davis/Research/triangular_Hubbard/desktop_files/triangular_Hubbard/nlceforhubbardontriangularlattice/Hubbard_data/"*name*"_$(N)_$(NTOP)"*".dat",
+        "r",
+    ) do io
+        quantities=readdlm(io, skipstart=1) # skip the first line
+    end
+    E::Array{Float64, 1} = quantities[:, 1]
+    Esq::Array{Float64, 1} = quantities[:, 2]
+    M::Array{Float64, 1} = quantities[:, 3]
+    Msq::Array{Float64, 1} = quantities[:, 4]
+    N_tot::Array{Float64, 1} = quantities[:, 5]
+    # calculate thermal average
+    β = 1/T
+    Z::Float64 = 0
+    E_avg::Float64 = 0
+    Esq_avg::Float64 = 0
+    M_avg::Float64 = 0
+    Msq_avg::Float64 = 0
+    N_tot_avg::Float64 = 0
+    for (n, En) in enumerate(E)
+            P = exp(-β*En)
+            Z += P
+            E_avg += En * P
+            Esq_avg += Esq[n] * P
+            M_avg += M[n] * P
+            Msq_avg += Msq[n] * P
+            N_tot_avg += N_tot[n] * P
+    end
+    return [Z E_avg Esq_avg M_avg Msq_avg N_tot_avg]
+end
+
+quant_names = ['E' "Esq" 'M' "Msq" "N_tot"]
+#=
+quantities = E_Quants(N=N, sectors_info=sectors_info, bonds=bonds)
+printing(quantities; Quant_names = quant_names, name = "test", NTOP=NTOP)
+thermal_avg(;T=T, name = "test", NTOP=NTOP)=#
